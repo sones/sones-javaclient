80207ec8f5342c43fd6a98ebd07563a3f7785e63
Daniel Kirstenpfad <daniel@sones.de>
2011-06-16 13:53:48 +0200

    * changed license from AGPLv3 to LGPL v3

--- src/de/sones/GraphDBClient/QueryResult/ResultType.java
+++ src/de/sones/GraphDBClient/QueryResult/ResultType.java
@@ -1,21 +1,22 @@
 /*
-* sones GraphDB(v2) - OpenSource Graph Database - http://www.sones.com
-* Copyright (C) 2007-2011 sones GmbH
+* sones GraphDB Java Client Library 
+* Copyright (C) 2007-2011 sones GmbH - http://www.sones.com
 *
-* This file is part of sones GraphDB Community Edition.
-*
-* sones GraphDB OSE is free software: you can redistribute it and/or modify
-* it under the terms of the GNU Affero General Public License as published by
-* the Free Software Foundation, version 3 of the License.
-*
-* sones GraphDB OSE is distributed in the hope that it will be useful,
+* This library is free software; you can redistribute it and/or
+* modify it under the terms of the GNU Lesser General Public
+* License as published by the Free Software Foundation; either
+* version 2.1 of the License, or (at your option) any later version.
+* 
+* This library is distributed in the hope that it will be useful,
 * but WITHOUT ANY WARRANTY; without even the implied warranty of
-* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-* GNU Affero General Public License for more details.
-*
-* You should have received a copy of the GNU Affero General Public License
-* along with sones GraphDB OSE. If not, see <http://www.gnu.org/licenses/>.
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+* Lesser General Public License for more details.
+* 
+* You should have received a copy of the GNU Lesser General Public
+* License along with this library; if not, write to the Free Software
+* Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
 */
+
 package de.sones.GraphDBClient.QueryResult;
 /**
  * Enum that specifies the result of a query
